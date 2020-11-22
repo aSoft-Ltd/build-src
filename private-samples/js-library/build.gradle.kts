99f3e6c2fe698ba37fec85ec12b5f3d3dd0a6b1e
andylamax <andylamax@programmer.net>
2020-11-23 01:10:32 +0300

    Added a private-sample for js

--- private-samples/js-library/build.gradle.kts
+++ private-samples/js-library/build.gradle.kts
@@ -1,12 +1,15 @@
 plugins {
     kotlin("js") version "1.4.10"
     id("tz.co.asoft.library")
+    id("io.codearte.nexus-staging") version "0.22.0"
+    signing
 }
 
 val lib_version = "0.0.1"
 group = "tz.co.asoft"
 version = lib_version
 
-dependencies {
-    implementation(kotlin("stdlib"))
-}
+aSoftLibrary(
+    version = lib_version,
+    description = "A test lib"
+)
